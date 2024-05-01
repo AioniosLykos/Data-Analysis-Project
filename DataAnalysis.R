28b8d99a07ce5ab5fdc63251494c2f0ed59b3a8e
AioniosLykos <168579548+AioniosLykos@users.noreply.github.com>
2024-05-01 17:57:44 -0400

    previous message

--- DataAnalysis.R
+++ DataAnalysis.R
@@ -1,6 +1,8 @@
 library(readr)
 library(tidyr)
 library(dplyr)
+library(stringr)
+
 FFdata_original <- read_delim("~/AnalysisProject/FastFoodNutritionMenuV3.csv")
 diet_original <- read_delim("~/AnalysisProject/Diet.csv")
 activity_original <- read_delim("~/AnalysisProject/ActivityCategories.csv")
@@ -10,21 +12,8 @@ cal_Per_GrOfProtein <- 4
 cal_Per_GrOfFat <- 9
 cal_Per_GrOfCarbs <- 4
 
-data <- FFdata_original %>%
-  mutate_at(vars(-column1, -column2), ~ifelse(is.na(.), 0, .)) %>%
-  mutate_at(vars(-column1, -column2), ~ifelse(. == "", 0, .))
 
-# Replace non-integer values < 5 with 5
-data <- data %>%
-  mutate_at(vars(-column1, -column2), ~ifelse(!is.na(.) & !is.integer(.) & . < 5, 5, .))
-
-
-
-
-
-
-
-
-
-
-
+data <- FFdata_original %>% convert_to_numeric %>%
+  mutate(across(-c("Company", "Item"), ~ifelse(is.na(.) , 0, .))) %>%
+  mutate(across(-c("Company", "Item"), ~ifelse( . == "<5" , 5, .)))  %>%
+  mutate(across(-c("Company", "Item"), ~ifelse( . == "<1" , 1, .)))
