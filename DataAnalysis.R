7fbec260fa3bba7126c8c6da4f13dc2434d82b01
AioniosLykos <168579548+AioniosLykos@users.noreply.github.com>
2024-05-01 17:57:00 -0400

    NA and <5,<1 are dealt with by changing into 0,5,1 respectively
    Still need to deal with rows containing special characters -> to be removed

--- DataAnalysis.R
+++ DataAnalysis.R
@@ -1,16 +1,22 @@
 library(readr)
-FFdata <- read_delim("~/AnalysisProject/FastFoodNutritionMenuV3.csv")
-diet <- read_delim("~/AnalysisProject/Diet.csv")
-activity <- read_delim("~/AnalysisProject/ActivityCategories.csv")
+library(tidyr)
+library(dplyr)
+FFdata_original <- read_delim("~/AnalysisProject/FastFoodNutritionMenuV3.csv")
+diet_original <- read_delim("~/AnalysisProject/Diet.csv")
+activity_original <- read_delim("~/AnalysisProject/ActivityCategories.csv")
 
+caloriesAllowed <- 700
+cal_Per_GrOfProtein <- 4
+cal_Per_GrOfFat <- 9
+cal_Per_GrOfCarbs <- 4
 
+data <- FFdata_original %>%
+  mutate_at(vars(-column1, -column2), ~ifelse(is.na(.), 0, .)) %>%
+  mutate_at(vars(-column1, -column2), ~ifelse(. == "", 0, .))
 
-
-
-
-
-
-
+# Replace non-integer values < 5 with 5
+data <- data %>%
+  mutate_at(vars(-column1, -column2), ~ifelse(!is.na(.) & !is.integer(.) & . < 5, 5, .))
 
 
 
