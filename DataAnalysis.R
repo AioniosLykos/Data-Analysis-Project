9c2528e0c74316d7bce76bf15752ceec53c7f44c
AioniosLykos <168579548+AioniosLykos@users.noreply.github.com>
2024-05-03 11:52:30 -0400

    "MAE: 16.242380952381" "MSE: 2774.48357142857" "RMSE: 52.6733668131113" "R-squared: 0.9398870962613" "Percent Error: 10.3100245678427 %" "Variance: 2513.0620248309" "Standard Deviation: 50.1304500760855"
    estimation based on given grams of fat, protein and carbs.
    -> will try adding substracting fiber calories

--- DataAnalysis.R
+++ DataAnalysis.R
@@ -29,51 +29,56 @@ data <- FFdata_original %>%
 data<- data %>%
   mutate(across(-c("Company", "Item"), ~{ifelse(grepl("\\d+\\.\\d+", .), .,
                                                 ifelse(grepl("[^A-Za-z0-9.]", .), -1, .))
-                                        } ) )
+  } ) )
 
 #change necessary column types to numeric
 data <- data %>%
   mutate_at(vars(-c("Company", "Item")), as.numeric)
 
-sapply(diet, class)
+sapply(data, class)
 
 #Remove spaces from column names
 names(data) <- gsub("\\s+", "", names(data))
 names(diet) <- gsub("\\s+", "", names(diet))
 
+#Remove items that have no effect on the calorie budget or we have no information on it.
+data <- data %>% filter(.$Calories != -1 ) %>% filter(.$Calories != 0 )
+#Remove items that are not named or do not have the company name 
+data <- data %>% filter(.$Item != "", .$Company != "")
+
 #new dataset where we only see calories, total fat,protein and carbs as well as the % of each
 macro_col <- c("Company","Item","Calories","TotalFat(g)","Carbs(g)","Protein(g)")
-macros <- data %>% select(all_of(macro_col))
-macros <- macros %>%
-         #calFromFat_S means we are using scientific value and not data$CaloriesfromFat (which is not too far off)
-                mutate(calFromFat_S = .$`TotalFat(g)`*cal_Per_GrOfFat,
-                       calFromCarbs_S = .$`Carbs(g)`*cal_Per_GrOfCarbs,
-                       calFromProtein_S = .$`Protein(g)`*cal_Per_GrOfProtein) %>%
-                mutate(TotalCalories_S = .$calFromProtein_S + .$calFromCarbs_S + .$calFromFat_S)
+macros_1 <- data %>% select(all_of(macro_col))
+macros_1 <- macros_1 %>%
+  #calFromFat_S means we are using scientific value and not data$CaloriesfromFat (which is not too far off)
+  mutate(calFromFat_S = .$`TotalFat(g)`*cal_Per_GrOfFat,
+         calFromCarbs_S = .$`Carbs(g)`*cal_Per_GrOfCarbs,
+         calFromProtein_S = .$`Protein(g)`*cal_Per_GrOfProtein) %>%
+  mutate(TotalCalories_S = .$calFromProtein_S + .$calFromCarbs_S + .$calFromFat_S)
 
 # Mean Absolute Error (MAE)
-mae <- mean(abs(macros$TotalCalories_S - macros$Calories))
+mae <- mean(abs(macros_1$TotalCalories_S - macros_1$Calories))
 
 # Mean Squared Error (MSE)
-mse <- mean((macros$TotalCalories_S - macros$Calories)^2)
+mse <- mean((macros_1$TotalCalories_S - macros_1$Calories)^2)
 
 # Root Mean Squared Error (RMSE)
 rmse <- sqrt(mse)
 
 # Coefficient of Determination (R-squared)
-r_squared <- 1 - (sum((macros$Calories - macros$TotalCalories_S)^2) / sum((macros$Calories - mean(macros$Calories))^2))
+r_squared <- 1 - (sum((macros_1$Calories - macros_1$TotalCalories_S)^2) / sum((macros_1$Calories - mean(macros_1$Calories))^2))
 
 # Percent Error
 # Check if denominator (real) is not zero before calculating percent error
-if (all(macros$Calories != 0)) {
-  percent_error <- mean(abs((macros$TotalCalories_S - macros$Calories) / macros$Calories)) * 100
+if (all(macros_1$Calories != 0)) {
+  percent_error <- mean(abs((macros_1$TotalCalories_S - macros_1$Calories) / macros_1$Calories)) * 100
 } else {
   # Exclude cases where actual values are zero from the percent error calculation
-  percent_error <- mean(abs((macros$TotalCalories_S - macros$Calories) / macros$Calories[macros$Calories != 0])) * 100
+  percent_error <- mean(abs((macros_1$TotalCalories_S - macros_1$Calories) / macros_1$Calories[macros_1$Calories != 0])) * 100
 }
 
 # Calculate variance of absolute differences
-variance <- var(abs(macros$Calories - macros$TotalCalories_S))
+variance <- var(abs(macros_1$Calories - macros_1$TotalCalories_S))
 
 #Calculate Standard Deviation
 deviation <- sqrt(variance)
@@ -86,9 +91,6 @@ print(paste("R-squared:", r_squared))
 print(paste("Percent Error:", percent_error, "%"))
 print(paste("Variance:", variance))
 print(paste("Standard Deviation:", deviation))
-
-
-
 
 
 
